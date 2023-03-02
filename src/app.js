c1bc9f29f5e5acd9cd5409dec1772c7045db4a58
adcavalcant <aderc19@gmail.com>
2023-03-02 14:43:03 -0300

    Adicionando o método GET por ID e o PUT

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   { id: 1, titulo: "Senhor dos Anéis" },
   { id: 2, titulo: "O Hobbit" },
 ];
 
-app.get("/", (req, res) => {
-  res.status(200).send("Olá, Mundo!");
-});
-
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
+
+app.delete("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros.splice(livros[index]);
+  res.json(livros);
 });
 
 export default app;
