ef9d3a9b4b3a55c5189847cf84be4700a7f42163
adcavalcant <aderc19@gmail.com>
2023-03-01 21:53:54 -0300

    Ajustes para início da API de livros

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import app from "./src/app.js";
-
-const port = process.env.port; //process.env.port = porta pra produção.
+const port = process.env.port || 3000; //process.env.port = porta pra produção.
 
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
