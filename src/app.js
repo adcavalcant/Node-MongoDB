d9768c50f5bc5c498e584afd29ad1c174e7b507b
adcavalcant <aderc19@gmail.com>
2023-03-03 19:51:30 -0300

    Início da criação do Controller de Livros

--- src/app.js
+++ src/app.js
@@ -1,25 +1,40 @@
 import express from "express";
-import mongoose from "mongoose";
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
+
+db.on("error", console.log.bind(console, "Erro de conexão"));
+
+db.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso!");
+});
+
 const app = express();
 
 app.use(express.json());
 
-const livros = [
-  { id: 1, titulo: "Senhor dos Anéis" },
-  { id: 2, titulo: "O Hobbit" },
-];
+// const livros = [
+//   { id: 1, titulo: "Senhor dos Anéis" },
+//   { id: 2, titulo: "O Hobbit" },
+// ];
 
 app.get("/", (req, res) => {
-  res.send('Tá funcionando a API ;)');
+  res.send("Tá funcionando a API ;)");
 });
 
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+  livros
+    .find()
+    .then((livros) => {
+      res.status(200).json(livros);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Erro ao buscar livros." });
+    });
 });
 
 app.get("/livros/:id", (req, res) => {
-  let index = buscaLivro(req.params.id);
-  res.json(livros[index]);
+  
 });
 
 app.post("/livros", (req, res) => {
@@ -34,7 +49,7 @@ app.put("/livros/:id", (req, res) => {
 });
 
 function buscaLivro(id) {
-  return livros.findIndex((livro) => livro.id == id);
+  return livros.find((livro) => livro.id == id);
 }
 
 app.delete("/livros/:id", (req, res) => {
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-  "mongodb+srv://adcavalcant:123@cluster0.xkmttgz.mongodb.net/alura-node"
-);
+mongoose.connect("mongodb+srv://alura:123@alura.4vllcau.mongodb.net/alura-node");
 
-const db = mongoose.connection;
+let db = mongoose.connection;
 
 export default db;
