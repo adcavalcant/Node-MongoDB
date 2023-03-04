456389c85d8554449a64b432379b56ab656758d4
adcavalcant <aderc19@gmail.com>
2023-03-04 18:13:22 -0300

    Implementando as rotas de Pensamentos para adicionar no projeto FrontEnd Memoteca desenvolvido em Angular

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
 import autores from "./autoresRoutes.js";
+import pensamentos from "./pensamentosRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
@@ -9,6 +10,7 @@ const routes = (app) => {
 
   app.use(express.json(), livros);
   app.use(express.json(), autores);
+  app.use(express.json(), pensamentos);
 };
 
 export default routes;
